46c95a03e8286228a0387c9ab5b806e115f0259f
anshulpatel25 <anshulpatel25@gmail.com>
2015-12-21 11:01:27 +0530

    Adding attributes

--- recipes/machine.rb
+++ recipes/machine.rb
@@ -1,17 +1,17 @@
 require 'chef/provisioning/aws_driver'
 
-with_driver 'aws::us-west-2'
+with_driver node[:aws_machine][:region]
 
 
-machine 'rhel-test5' do
+machine node[:aws_machine][:machine_name] do
 	recipe 'aws_machine::vim'
 	machine_options :bootstrap_options => {
-	:key_name => 'anshul_aws',
-	:instance_type => 't2.micro',
-	:associate_public_ip_address => true,
-	:image_id => 'ami-775e4f16',
-	:security_group_ids => 'sg-05a08f61',
-	:subnet_id => 'subnet-1928cd40'
+	:key_name => node[:aws_machine][:key_name],
+	:instance_type => node[:aws_machine][:instance_type],
+	:associate_public_ip_address => node[:aws_machine][:public_ip],
+	:image_id => node[:aws_machine][:image_id],
+	:security_group_ids => node[:aws_machine][:security_group_ids],
+	:subnet_id => node[:aws_machine][:subnet_id]
 },
-	:ssh_username => 'ec2-user'
+	:ssh_username => node[:aws_machine][:ssh_username]
 end
